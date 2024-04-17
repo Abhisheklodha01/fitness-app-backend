4795fdc6f690029596d6c9eb729c038336038c3b
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-17 09:10:34 +0530

    adding cors policy

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ import express from 'express'
 import connectDB from './db/index.js'
 import router from './routes/user.route.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-// app.use(express.urlencoded())
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
 connectDB()
 
